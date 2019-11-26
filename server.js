608073ebde0ca29344feec2c14be3a4a3bea4b09
jaszly <jasvmurray@gmail.com>
2019-11-26 12:08:01 +0700

    initial commit
    -set up server
    -install dependencies
    -create .env
    -git ignores

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+// const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// app.get('/controllers/root.js', (req, res) => {
+// 	res.send('Welcome to Curate API')
+// })   api >> react
+
+app.listen(process.env.PORT, () => {
+  console.log(`'ready on port' ${process.env.PORT}`);
+});
+
+module.exports = app;
