9c698e8dff08d745a9142a914ef0b90b2020b353
jaszly <jasvmurray@gmail.com>
2019-11-26 12:57:47 +0700

    create validation files

---
+++ validation/signup.js
@@ -0,0 +1,7 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+//notes
+// used to validate inputs (e.g. check for valid email format, confirming passwords match)
+// is-empty: global function that will come in handy when we use validator
+// jsonwebtoken: used for authorization
