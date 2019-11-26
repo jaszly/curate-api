359c274ad18c1c0bb750b43d067c9f7f7bb7c726
jaszly <jasvmurray@gmail.com>
2019-11-26 12:30:39 +0700

    add middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 // const port = process.env.PORT || 5000;
 
+const app = express();
+
+// bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 require("dotenv").config();
+require("./database");
 
 // app.get('/controllers/root.js', (req, res) => {
 // 	res.send('Welcome to Curate API')
