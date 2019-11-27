678c89bb82974cfb8870568e37f323048f3e31b4
jaszly <jasvmurray@gmail.com>
2019-11-28 02:33:36 +0700

    auth routes using passport

---
+++ router.js
@@ -0,0 +1,15 @@
+const Auth = require('./controllers/auth')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+//Checking Authentication status before visiting aparticular route
+const requireAuth = passport.authenticate('jwt', { session: false })
+const requireLogin = passport.authenticate('local', { session: false })
+
+module.exports = app => {
+	app.get('/', requireAuth, (req, res) => {
+		res.send({ Working: 'Yes' })
+	})
+	app.post('/login', requireLogin, Auth.login)
+	app.post('/signup', Auth.signup)
+}
